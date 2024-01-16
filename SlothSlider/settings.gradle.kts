20bbbfd3d9e9ecb382165300a33ec86cb490b86f
Martin Amor <mamorg22@fpcoruna.afundacion.org>
2024-01-16 09:38:05 +0100

    Proyecto creado

---
+++ SlothSlider/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Sloth Slider"
+include(":app")
+ 
